b3ecee3bf34cc05881d56ab69e6b4af259836e93
samurrai <samurrrai.r@gmail.com>
2019-01-27 10:45:15 +0600

    Практика по свойствам

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertysPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeOfArray<int> array = new RangeOfArray<int>(7);
+            array.LowerBound = 3;
+            array.UpperBound = 10;
+            array[4] = 213;
+            Console.WriteLine(array[4]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RangeOfArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertysPractice
+{
+    
+    public class RangeOfArray<T>
+    {
+        private T[] arr;
+        public int LowerBound { get; set; }
+        public int UpperBound { get; set; }
+
+        public RangeOfArray(int size)
+        {
+            arr = new T[size];
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                if (i > LowerBound && i < UpperBound)
+                {
+                    return arr[i - LowerBound];
+                }
+                return arr[0];
+            }
+
+            set
+            {
+                if (i > LowerBound && i < UpperBound)
+                {
+                    arr[i - LowerBound] = value;
+                }
+            }
+        }
+    }
+}
